8eccdbedc8c2f51ef1c7858bfa9a55b91cc88d27
marvall (Yaroslav Bezugliy) <marvallua@gmail.com>
2022-11-15 12:53:34 +0200

    init module-06-lesson-11

--- js/index.js
+++ js/index.js
@@ -1,334 +1,256 @@
 'use strict';
+//* Об'єкти window і document
+// console.log(window);
+// console.log(document);
+// console.log(location);
+// console.log(navigator);
 
-/*
- * Ланцюжки прототипів.
- * Object.create().
- * obj.hasOwnProperty()
+//* Cтарі методи пошуку елементів (getElementBy...)
+
+// const titleElement = document.getElementById('title');
+// const listItemElement = document.getElementsByClassName('list__item');
+
+// console.log(titleElement);
+// console.log(listItemElement);
+// * Сучасні методи пошуку елементів (querySelector, querySelectorAll)
+
+// const ref = {
+//   titleElement: document.querySelector('#title'),
+//   listItemElements: document.querySelectorAll('.list__item'),
+// };
+
+// const titleElement = document.querySelector('#title');
+// const listItemElements = document.querySelectorAll('.list__item');
+
+// // console.log(ref.titleElement);
+// console.log(listItemElements);
+
+//* Навігації по DOM дереву
+// elem.parentNode - вибере батьківський elem.
+// elem.childNodes - псевдомасив, зберігає всі дочірні елементи, включно з текстовими.
+// elem.children - псевдомасив, зберігає тільки дочірні вузли-елементи, тобто ті, що відповідають тегам.
+// elem.firstChild - вибере перший дочірній елемент всередині elem, включно з текстовими вузлами.
+// elem.firstElementChild - вибере перший дочірній вузол-елемент всередині elem.
+// elem.lastChild - вибере останній дочірній елемент всередині elem, включно з текстовими вузлами.
+// elem.lastElementChild - вибере останній дочірній вузол-елемент всередині elem.
+// elem.previousSibling - вибере елемент «зліва» від elem (його попереднього сусіда).
+// elem.previousElementSibling - вибере вузол-елемент «зліва» від elem (його попереднього сусіда).
+// elem.nextSibling - вибере елемент «праворуч» від elem (його наступного сусіда)
+// elem.nextElementSibling - вибере вузол-елемент «праворуч» від elem (його наступного сусіда).
+
+// const listElement = document.querySelector('.list');
+// console.log(listElement.nextElementSibling);
+/**
+ * Атрибути як властивості.
  */
 
-// const user = {
-//   firstName: 'Jhon',
-//   age: 40,
-//   sayHello() {
-//     console.log(
-//       `${this.firstName ? this.firstName : ''} ${
-//         this.lastName ? this.lastName : ''
-//       } say hello...`
-//     );
-//   },
-// };
+// const imageElement = document.querySelector('#baseImage');
+// console.log(imageElement);
+// imageElement.src = 'https://picsum.photos/id/6/200/300';
+// imageElement.title = 'notebook';
+// imageElement.alt = 'notebook';
+// imageElement.width = 300;
 
-// // console.log('user: ', user);
-// // user.sayHello();
-
-// const james = Object.create(user);
-// james.firstName = 'James';
-// james.lastName = 'Jonson';
-// // james.sayHello();
-// // console.log('james: ', james);
-
-// const eric = Object.create(james);
-// eric.firstName = 'Eric';
-// eric.age = 30;
-// // eric.sayHello();
-// console.log('eric: ', eric.hasOwnProperty('lastName'));
-
-// eric.__proto__ = user;
-// console.log(eric);
-// eric.sayHello = function () {
-//   console.log('HI HIHI HI H');
-// };
-// eric.__proto__.sayHello = function () {
-//   console.log(this);
-// };
-// eric.sayHello();
-
-// const fn = function () {};
-// console.dir(fn);
-// console.log(fn.hasOwnProperty('name'));
-// https://learn.javascript.ru/article/native-prototypes/native-prototypes-classes.svg
-
-//* Власні та не власні властивості об'єкта
-
-//* Перевірка власних властивостей obj.hasOwnProperty()
-// console.log(eric.hasOwnProperty('age'));
-// console.log(eric);
-
-// // * Цикл for...in, перебирає і власні, і не власні властивості.
-// for (const property in eric) {
-//   //   if (eric.hasOwnProperty(property)) {
-//   //     console.log(`${property}: ${eric[property]}`);
-//   //   }
-//   console.log(
-//     'property : ',
-//     property,
-//     '  is own: ',
-//     eric.hasOwnProperty(property)
-//   );
-// }
-// console.log(eric);
-/*
- * Клас - шаблон, за яким будуть створені однотипні об'єкти
- * Екземпляр - це об'єкт створений за класом
- * Інтерфейс - набір доступних методів та властивостей
- */
-
-//? Функція конструктор
-// const Person = function ({
-//   firstName,
-//   lastName,
-//   age,
-//   hairColor,
-//   eyesColor,
-// } = {}) {
-//   // this = {};
-//   this.firstName = firstName;
-//   this.lastName = lastName;
-//   this.age = age;
-//   this.legs = 2;
-//   this.hands = 2;
-//   this.eyes = 2;
-//   this.hairColor = hairColor;
-//   this.eyesColor = eyesColor;
-//   // return this;
-// };
-
-// // console.dir(Person);
-
-// Person.prototype.changeHairColor = function (newHairColor) {
-//   this.hairColor = newHairColor;
-// };
-
-// Person.prototype.changeFirstName = function (newName) {
-//   this.firstName = newName;
-// };
-
-// const person = new Person({
-//   firstName: 'Oleksii',
-//   lastName: 'Repin',
-//   age: 30,
-//   hairColor: 'black',
-//   eyesColor: 'brown',
+// https://picsum.photos/200/300
+// const actionButtonElement = document.querySelector('.action');
+// actionButtonElement.addEventListener('click', e => {
+//   imageElement.src = `https://picsum.photos/id/${Math.floor(
+//     Math.random() * 1000
+//   )}/200/300`;
+//   imageElement.alt = 'random image';
 // });
-
-// person.changeHairColor('brown');
-// person.changeFirstName('Susie');
-
-// const person2 = new Person({
-//   firstName: 'Emily',
-//   lastName: 'Henderson',
-//   age: 20,
-//   hairColor: 'pink',
-//   eyesColor: 'blue',
-// });
-
-// person2.changeFirstName('Teresa');
-
-// console.log(person);
-// console.log(person2);
 
 /*
- * Класи: оголошення, конструктор, методи (class, constructor)
- * Приватні властивості та методи // умовно приватні властивості.
- * Статичні властивості та методи
- * Геттери та сеттери
- * Створити класс User.
+ * Доступ до атрибутів
+ *
+ * elem.hasAttribute(name)
+ * elem.getAttribute(name)
+ * elem.setAttribute(name, value)
+ * elem.removeAttribute(name)
+ * elem.attributes
  */
 
-// class Person {
-//   #id;
-//   static multiply = 2;
-//   #age = 1;
-//   constructor(firstName, lastName) {
-//     this.#id = Math.random().toString(36).substring(2, 9);
-//     // this._currentDate = new Date(); // умовно приватні властивості
-//     this.firstName = firstName;
-//     this.lastName = lastName;
-//   }
+// const imageElement = document.querySelector('#baseImage');
+// // console.log(imageElement.removeAttribute('class'));
+// console.log(imageElement.hasAttribute('class'));
+// console.log(imageElement.getAttribute('class'));
 
-//   get age() {
-//     return this.#age;
-//   }
+/**
+ *  Об'єкт style, cssText
+ */
+// const titleElement = document.getElementById('title');
+// console.log(titleElement);
+// titleElement.style.color = '#777777';
+// titleElement.style.fontSize = '50px';
+// titleElement.style.backgroundColor = '#caffdd';
+// titleElement.style.padding = '20px';
 
-//   set age(param) {
-//     if (typeof param === 'number') {
-//       this.#age = param;
-//     } else if (Number(param)) {
-//       this.#age = Number(param);
-//     }
-//   }
+// titleElement.style.cssText = `color: #777777;
+//   font-size: 30px;
+//   background-color: #caffdd;
+//   padding: 20pх
+//   `;
+// titleElement.style = `color: #777777;
+// font-size: 30px;
+// background-color: #caffdd;
+// padding: 20pх
+// `;
 
-//   //   sayHello() {
-//   //     console.log(
-//   //       `${this.firstName ? this.firstName : ''} ${
-//   //         this.lastName ? this.lastName : ''
-//   //       } say Hello. PersonId = ${this.#getPersonId()}`
-//   //     );
-//   //   }
+// console.log(titleElement);
 
-//   //   #getPersonId() {
-//   //     return this.#id;
-//   //   }
+//* Методи об'єкту classList (add, remove, toggle, contains)
 
-//   //   static multiplyTo2(number) {
-//   //     return number * this.multiply;
-//   //   }
+// const rootElement = document.querySelector('.root');
+// console.log(rootElement.classList);
+// rootElement.classList.add('red');
+// rootElement.classList.remove('red');
+// console.log(rootElement.classList.contains('red'));
+// console.log(rootElement.className);
+// rootElement.classList.toggle('green');
 
-//   //   _geCurrentData() { // умовно приватний метод
-//   //     return this._currentDate;
-//   //   }
-// }
+// const modalElement = document.querySelector('.modal');
+// const openModalButton = document.querySelector('.openModal');
+// const closeModalButton = document.querySelector('.closeModal');
 
-// const person = new Person('John', 'Smith');
-// // person.sayHello();
-// person.age = 'asda';
-// console.log(person.age);
+// const callback = () => {
+//   modalElement.classList.toggle('hidden');
+// };
+
+// openModalButton.addEventListener('click', callback);
+// closeModalButton.addEventListener('click', callback);
+
+/**
+ * Card of Gallery
+ */
+
 /*
- * Наслідування з extends та super. Створити класс Сlient та Employeer наслідуючі класс User.
- * Створити класс Developer та Manager наслідуючі класс Employeer.
+<li class="gallery-item">
+  <a href="#">
+    <img src="https://picsum.photos/id/237/200/300" alt="Labrador">
+    <p>Some Description</p>
+  </a>
+</li>
+*/
+
+// const galleryRoot = document.querySelector('#rootList');
+
+// const makeGalleryItem = function () {
+//   const li = document.createElement('li');
+//   li.classList.add('gallery-item');
+
+//   const a = document.createElement('a');
+//   a.href = '#';
+//   li.append(a);
+
+//   const img = document.createElement('img');
+//   img.src = 'https://picsum.photos/id/237/200/300';
+//   img.alt = 'random image';
+//   a.append(img);
+//   const p = document.createElement('p');
+//   p.textContent = 'Some Description';
+//   a.append(p);
+//   return li;
+// };
+// galleryRoot.append(makeGalleryItem());
+
+/**
+ * Створити галерею на основі масиву pictures
  */
 
-// class User {
-//   constructor(props) {
-//     this.login = props.login;
-//     this.password = props.password;
-//     this.type = 'user';
-//   }
+/*
+  <li class="gallery-item">
+    <a href="#">
+      <img src="https://picsum.photos/id/237/200/300" alt="Labrador" width="" height="">
+      <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non laudantium nostrum, aliquid impedit sit repellendus neque minima
+            repudiandae nulla sed dolorem dolore corporis consectetur quam magni
+            vel numquam. Ipsam, ut?
+      </p>
+    </a>
+  </li>
+  */
 
-//   sayHowIs() {
-//     console.log(`I am "${this.type}"`);
-//   }
-// }
+// Створити карточку на основі document.createElement
+// Перебір масиву pictures через map
+// Вставка колекції карток на сторінку за допомогою append(...args);
 
-// const user = new User({
-//   login: 'johnsmith',
-//   password: '11111111',
-// });
+//pictures
+// console.log(pictures[0]);
+// const galleryRoot = document.querySelector('#rootList');
+// const makeGalleryItem = function ({
+//   classList,
+//   width,
+//   alt,
+//   description,
+//   height,
+//   url,
+// }) {
+//   const li = document.createElement('li');
+//   li.classList.add(classList);
 
-// // // user.sayHowIs();
+//   const a = document.createElement('a');
+//   a.href = '#';
+//   li.append(a);
 
-// // class Client extends User {
-// //   constructor(props) {
-// //     super(props);
-
-// //     this.discont = props.discont;
-// //     this.type = 'clinent';
-// //   }
-
-// //   buyProduct() {
-// //     console.log('buyProduct');
-// //   }
-
-// //   getCard() {
-// //     console.log('getCard');
-// //   }
-
-// //   pay() {
-// //     console.log('pay');
-// //   }
-// // }
-
-// // const client = new Client({
-// //   login: 'somePeople',
-// //   password: '112211',
-// //   discont: '20%',
-// // });
-
-// // // console.log(client);
-// // // client.sayHowIs();
-
-// class Employeer extends User {
-//   #salary;
-//   constructor(props) {
-//     super(props);
-
-//     this.#salary = props.salary;
-//     this.type = 'employeer';
-//   }
-
-//   getAccessToAdmin() {
-//     console.log('getAccessToAdmin');
-//   }
-
-//   getSalary() {
-//     return this.#salary;
-//   }
-// }
-
-// const employeer = new Employeer({
-//   login: 'Eric0001',
-//   password: '112211',
-//   salary: 1000,
-// });
-
-// // // console.log(client);
-// // // employeer.sayHowIs();
-
-// class Manager extends Employeer {
-//   constructor(props) {
-//     super(props);
-
-//     this.type = 'manager';
-//   }
-
-//   getAllSalaryList() {
-//     console.log('getAllSalaryList');
-//   }
-// }
-
-// const manager = new Manager({
-//   login: 'Steav888',
-//   password: '11234511',
-//   salary: 1200,
-// });
-
-// // manager.sayHowIs();
-
-// class Developer extends Employeer {
-//   constructor(props) {
-//     super(props);
-
-//     this.type = 'developer';
-//   }
-
-//   getAccessToTickList() {
-//     console.log(getAccessToTickList);
-//   }
-
-//   sayHowIs() {
-//     console.log('I am 100% Dev');
-//   }
-// }
-
-// const developer = new Developer({
-//   login: 'Jonson',
-//   password: '112342221',
-//   salary: 2000,
-// });
-
-// developer.sayHowIs();
-// console.log(developer.getSalary());
-
-// const users = [user, client, employeer, manager, developer];
-// console.log(developer);
-
-// developer.setSome = 'asda';
-// Developer.prototype.sayHowIs = function () {
-//   console.log(11);
+//   const img = document.createElement('img');
+//   img.src = url;
+//   img.alt = alt;
+//   img.width = width;
+//   img.height = height;
+//   a.append(img);
+//   const p = document.createElement('p');
+//   p.textContent = description;
+//   a.append(p);
+//   return li;
 // };
+// const elements = pictures.map((el, index, array) => makeGalleryItem(el));
+// const elements = pictures.map(makeGalleryItem);
+// galleryRoot.append(...pictures.map(makeGalleryItem));
 
-// developer.sayHowIs();
-// manager.sayHowIs();
-// console.dir(developer);
-// console.log(developer);
-// console.dir(Developer.prototype);
+// Створити карточку на основі шаблонної строки
+// Вставка колекції карток на сторінку за допомогою innerHTML;
+// Вставка колекції карток на сторінку за допомогою insertAdjacentHTML('afterbegin', [array].join(''));
 
-// console.dir(Developer.__proto__);
+// console.log(pictures);
 
-// console.dir(Developer);
-
-// const obj = {
-//   name: 'Some',
+// const galleryRoot = document.querySelector('#rootList');
+// const makeGalleryItem = function (picture) {
+//   return `
+//   <li class="${picture.classList}">
+//     <a href="#">
+//       <img
+//         src="${picture.url}"
+//         alt="${picture.alt}"
+//         width="${picture.width}"
+//         height="${picture.height}"
+//       >
+//       <p>${picture.description}</p>
+//     </a>
+//   </li>
+//   `;
 // };
+// console.log(galleryRoot.innerHTML);
+// // const elements = pictures.map((el, index, array) => makeGalleryItem(el));
+// // galleryRoot.innerHTML = elements.join('');
+// // galleryRoot.textContent = pictures.map(makeGalleryItem).join('');
+// galleryRoot.innerHTML = pictures.map(makeGalleryItem).join('');
+// console.log(pictures.map(makeGalleryItem).join(''));
 
-// console.log(obj);
+// const root = document.querySelector('.root');
+// root.innerHTML = `  <li class="gallery-item">
+// <a href="#">
+//   <img src="https://picsum.photos/id/237/200/300" alt="Labrador" width="" height="">
+//   <p>
+//         Lorem ipsum dolor sit amet consectetur adipisicing elit. Non laudantium nostrum, aliquid impedit sit repellendus neque minima
+//         repudiandae nulla sed dolorem dolore corporis consectetur quam magni
+//         vel numquam. Ipsam, ut?
+//   </p>
+// </a>
+// </li>`;
+
+// galleryRoot.insertAdjacentHTML(
+//   'afterbegin',
+//   pictures.map(makeGalleryItem).join('')
+// );
+
+// console.log(galleryRoot);
