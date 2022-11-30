c089520fc97db452ce9dd970413313f3d913ef69
marvall <marvallua@gmail.com>
2022-11-30 16:31:48 +0200

    init

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # javascript-bc-37
 
-Модуль 9 _______ Заняття 18
+Модуль 10 _______ Заняття 19
 
-1. Проміси.
+1. Кліент-Сервер Архітектура з точки зору адміністрування. Що є DNS як працює, на базі інтернет-провайдерів, хостинг-провайдерів. 
+Example Hosting: https://www.ukraine.com.ua/
+Example AdminPanel: https://adm.tools/domains/
+2. Протоколи http, https. Порты використання. [80, 443]. Як опрацьовуються порты.
+    - [nginx:80] (https://www.linode.com/docs/guides/how-to-configure-nginx/)
+    - [nginx:443] (http://nginx.org/en/docs/http/configuring_https_servers.html)
+    - [SSL] (https://www.google.com/search?q=certbot+nginx&rlz=1C1CHBD_ruUA833UA833&sxsrf=ALiCzsZppYsYplOgRaJjrTtksScHEI_S-w:1669815012691&source=lnms&tbm=vid&sa=X&ved=2ahUKEwiO3K_Vgdb7AhW9gv0HHQ-EClkQ_AUoAXoECAIQAw&biw=1366&bih=625&dpr=1#fpstate=ive&vld=cid:e9c66ef1,vid:ReXCT3M-uec)
+3. REST API. BACKEND PART, FRONT PART.
+4. [REAL_API] (https://binance-docs.github.io/apidocs/spot/en/)
+5. Що таке: Access-Control-Allow-Origin
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const cors = require('cors')
+
+const PORT = 3000;
+const HOST = 'http://localhost'
+
+const app = express()
+app.use(express.json());
+app.use(cors());
+
+const users = [
+    { id: 123456, login: 'john@admin.com', rules: "admin" },
+    { id: 122256, login: 'eric@admin.com', rules: "user" },
+    { id: 123356, login: 'james@admin.com', rules: "admin" },
+    { id: 124456, login: 'volly@admin.com', rules: "user" }
+]
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+    // res.send(users)
+})
+
+app.post('/', (req, res) => {
+    // try {
+    //     const { data } = req.body;
+    //     if (data) {
+    //         const user = {
+    //             id: Math.floor(Math.random() * 1000000),
+    //             ...data
+    //         }
+    //         users.push(user)
+    //         res.status(200).send("user added")
+    //     }
+    // } catch (e) {
+    //     res.status(500).send(e)
+    // }
+  res.send('Hello World!')
+})
+
+app.put('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.patch('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.delete('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${HOST}:${PORT}`)
+})
